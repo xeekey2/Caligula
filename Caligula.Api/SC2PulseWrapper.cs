68c60cec3dee0e2f734b008a46e277df1f520d6f
xeekey <xeekjaeger1@hotmail.com>
2024-08-07 22:06:03 +0200

    clean up

--- Caligula.Api/SC2PulseWrapper.cs
+++ Caligula.Api/SC2PulseWrapper.cs
@@ -11,6 +11,8 @@ using System.Web;
 
 namespace Caligula.Service
 {
+
+    //TODO: Remove this, and use existing functionality located in correct places.
     public class SC2PulseWrapper
     {
         private readonly HttpClient _httpClient;
@@ -18,26 +20,6 @@ namespace Caligula.Service
         public SC2PulseWrapper(string baseAddress)
         {
             _httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
-        }
-
-        public async Task<HttpResponseMessage> GetPlayerIdAsync(string playerName)
-        {
-            var response = await _httpClient.GetAsync($"api/character/search?term={playerName}");
-            if (response.IsSuccessStatusCode)
-            {
-                return response;
-            }
-            throw new InvalidOperationException("Failed to retrieve player ID.");
-        }
-
-        public async Task<HttpResponseMessage> GetGroupedProPlayerMatchHistoryAsync(int proPlayerId, string? date)
-        {
-            return await _httpClient.GetAsync($"api/group/match?proPlayerId={proPlayerId}&dateCursor={date}&typeCursor=_1V1");
-        }
-
-        public async Task<HttpResponseMessage> GetAllIdsFromProPlayerId(int proPlayerId)
-        {
-            return await _httpClient.GetAsync($"api/group/character/full?proPlayerId={proPlayerId}");
         }
 
         public async Task<HttpResponseMessage> GetNameFromId(int id)
